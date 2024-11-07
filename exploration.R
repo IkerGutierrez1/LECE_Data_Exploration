a7b3874b915389852bc52348288f2e87000aed0a
IkerGutierrez1 <173085374+IkerGutierrez1@users.noreply.github.com>
2024-11-07 19:11:52 +0100

    Movido codigo de excel a csv a funcion, corregido lectura de datos

--- exploration.R
+++ exploration.R
@@ -1,55 +1,12 @@
-library(readxl)
-library(dplyr)
-library(purrr)
-library(plotly)
-library(htmlwidgets)
-
+source("functions.R")
+#Data from excel to csv
 path <- "input/wetransfer_meteo-lece_01_2024-xlsx_2024-11-04_1640"
+extract_excels_to_csv(excel_path = path)
+#Load data
+df <- read.csv("input/data.csv")
+df$timestamp <- ifelse(grepl(" ", df$timestamp), df$timestamp, paste(df$timestamp, "00:00:00"))
 
-xlsx_names <- list.files(path = path, pattern = "\\.xlsx$", full.names = TRUE)
-
-df <- data.frame()
-for (excel in xlsx_names){
-  
-  df_sheets <- excel_sheets(excel)
-  print(paste("Procesando excel:", excel))
-  
-  df_test <- data.frame()
-  for (sheet in df_sheets) {
-    
-    
-    if (grepl("Datos", sheet)) {
-      temp_df <- read_excel(excel, sheet = sheet)
-      temp_df <- temp_df %>%
-        select(where(~ !all(is.na(.))))
-      
-      if (nrow(df_test) == 0) {
-        df_test <- temp_df
-      } else {
-        # Si df_test ya tiene datos, los unimos por la columna Fecha
-        df_test <- full_join(df_test, temp_df, by = "Fecha")
-      }
-  }
-  }
-  if (nrow(df) == 0) {
-    df <- df_test
-  }
-  else{
-    df <- bind_rows(df, df_test)}
-  df <- df[!is.na(df$Fecha), ]
-  }
-
-#Delete the auxiliar colum Tp and Met_Tpir_01 and created 81 colum, it is created because on of the sheets has columns which dont have values
-#but are taken into account, all of them are filter before except 81
-df <- df[, !(names(df) %in% c("Tp","Met_Idir_01", "...81"))]
-
-df <- df %>%
-  rename(timestamp = Fecha)
-
-df$timestamp <- as.POSIXct(df$timestamp, format = "%Y-%m-%d %H:%M:%S")
-
-write.csv(df, file = "input/data.csv", row.names = FALSE)
-
+#RDS files
 
 #Plots
 Tae_c <-get_columns_with_string(df,"Tae")
--- functions.R
+++ functions.R
@@ -1,3 +1,9 @@
+library(readxl)
+library(dplyr)
+library(purrr)
+library(plotly)
+library(htmlwidgets)
+
 NA_histogram <- function(df , path = "output/plots/", name = "na_histogram.html", linea = 500){
   
   # Asegúrate de que el directorio existe
@@ -84,4 +90,55 @@ get_columns_with_string <- function(dataframe, search_string,ignore_case = TRUE)
   matching_columns <- names(dataframe)[grepl(search_string, names(dataframe), ignore.case = ignore_case)]
   
   return(matching_columns)
+}
+
+extract_excels_to_csv <- function(excel_path = "input/wetransfer_meteo-lece_01_2024-xlsx_2024-11-04_1640",
+                                  save_dir = "input/" ,
+                                  save_name = "data.csv" ){
+  #Function to extract data from all excels in excel_path, it creates a .csv with all the data
+  xlsx_names <- list.files(path = excel_path, pattern = "\\.xlsx$", full.names = TRUE)
+  
+  df <- data.frame()
+  for (excel in xlsx_names){
+    
+    df_sheets <- excel_sheets(excel)
+    print(paste("Procesando excel:", excel))
+    
+    df_test <- data.frame()
+    for (sheet in df_sheets) {
+      
+      
+      if (grepl("Datos", sheet)) {
+        temp_df <- read_excel(excel, sheet = sheet)
+        temp_df <- temp_df %>%
+          select(where(~ !all(is.na(.))))
+        
+        if (nrow(df_test) == 0) {
+          df_test <- temp_df
+        } else {
+          # Si df_test ya tiene datos, los unimos por la columna Fecha
+          df_test <- full_join(df_test, temp_df, by = "Fecha")
+        }
+      }
+    }
+    if (nrow(df) == 0) {
+      df <- df_test
+    }
+    else{
+      df <- bind_rows(df, df_test)}
+    df <- df[!is.na(df$Fecha), ]
+  }
+  
+  #Delete the auxiliar colum Tp and Met_Tpir_01 and created 81 colum, it is created because on of the sheets has columns which dont have values
+  #but are taken into account, all of them are filter before except 81
+  df <- df[, !(names(df) %in% c("Tp","Met_Idir_01", "...81"))]
+  
+  df <- df %>%
+    rename(timestamp = Fecha)
+  
+  df$timestamp <- as.POSIXct(df$timestamp, format = "%Y-%m-%d %H:%M:%S")
+  
+  write.csv(df, file = paste0(save_dir,save_name), row.names = FALSE)
+  print("Data was saved")
+  
 }
